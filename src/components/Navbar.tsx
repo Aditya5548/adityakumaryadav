4785fd21772aebeb492429400c77d571849b7428
Aditya Kumar Yadav <yadityakumar205@gmial.com>
2025-09-15 12:57:57 +0530

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import image1 from '../image/1.png'
+import image1 from '../image/1.png';
+import img1 from '../image/call_icon.png';
+import img2 from '../image/whatsapp_icon.png';
 import Link from "next/link";
 const Navbar = () => {
     function togglebox() {
@@ -18,41 +20,44 @@ const Navbar = () => {
             }
         }
     }
-  return (
-      <section className="first-section">
-          <div className="container-fluid navbar-container">
-              <div className="navbar-image">
-                  <div><Image src={image1} alt="Logo" width={120} height={40} /></div>
-                  <div className="toogle-btn"><i className="fa-solid fa-bars" onClick={togglebox}></i></div>
-              </div>
-              <div  className="navbar-list">
-                  <ul className="navbar-links">
-                      <li>
-                          <Link href="/" className="link-tag">Home</Link>
-                      </li>
-                      <li>
-                          <Link href="/about" className="link-tag">About</Link>
-                      </li>
-                      <li>
-                          <Link href="/skills" className="link-tag">Skils</Link>
-                      </li>
-                      <li>
-                          <Link href="/project" className="link-tag">Projects</Link>
-                      </li>
-                      <li>
-                          <Link href="/contactus" className="link-tag">Contact</Link>
-                      </li>
-                      <li>
-                          <Link href="/resume.pdf" className="btn btn-danger link-tag text-light border-0">Resume</Link>
-                      </li>
-                  </ul>
-                  <div>
-                  </div>
-              </div>
-          </div>
-
-      </section>
-  );
+    return (
+        <section className="first-section">
+            <div className="container-fluid navbar-container">
+                <div className="navbar-image">
+                    <div><Image src={image1} alt="Logo" width={120} height={40} /></div>
+                    <div className="toogle-btn"><i className="fa-solid fa-bars" onClick={togglebox}></i></div>
+                </div>
+                <div className="navbar-list">
+                    <ul className="navbar-links">
+                        <li>
+                            <Link href="/" className="link-tag">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/about" className="link-tag">About</Link>
+                        </li>
+                        <li>
+                            <Link href="/skills" className="link-tag">Skils</Link>
+                        </li>
+                        <li>
+                            <Link href="/project" className="link-tag">Projects</Link>
+                        </li>
+                        <li>
+                            <Link href="/contactus" className="link-tag">Contact</Link>
+                        </li>
+                        <li>
+                            <Link href="/resume.pdf" className="btn btn-danger link-tag text-light border-0">Resume</Link>
+                        </li>
+                    </ul>
+                    <div>
+                    </div>
+                </div>
+            </div>
+            <div className="assistant">
+                <Link href="tel:+917376219758"><img className="img1 mx-2" src={img1.src} alt="" height={50} width={50}/></Link>
+                <Link href="https://wa.me/+919005825347"><img className='img2' src={img2.src} alt=""  height={65} width={65} /></Link>
+            </div>
+        </section>
+    );
 };
 
 export default Navbar;
